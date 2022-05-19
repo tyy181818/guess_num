7d22e99cc232b757efbc9299c85a9d470d36da53
Pei Fang <105428416+tyy181818@users.noreply.github.com>
2022-05-19 14:29:35 +0800

    Add files via upload

---
+++ guess_num.py
@@ -0,0 +1,15 @@
+import random
+from socket import NI_NUMERICHOST
+r = random.randint(1,100)
+
+while True :
+    num = input('1~100，請猜數字: ')
+    num = int(num)
+    if num == r :
+        print('恭喜你猜對了!')
+        break
+    elif num > r :
+        print('再小一點')
+    elif num < r :
+        print('再大一點')
+   
